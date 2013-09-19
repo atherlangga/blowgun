41f65e6ec601193588e9bbd0a71ddc5bf420f09f
Akbar Taufiq Herlangga <angga@atherlangga.com>
2013-09-19 07:23:13 +0700

    Add methods declaration for `VerticesBuilder`.

--- resources/code/libs/blowgun/vertices_builder.h
+++ resources/code/libs/blowgun/vertices_builder.h
@@ -1,11 +1,36 @@
 #ifndef BLOWGUN_VERTICES_BUILDER_H
 #define BLOWGUN_VERTICES_BUILDER_H
 
+#include <string>
+#include <memory>
+
+#include "types.h"
+#include "vertices.h"
+
 namespace blowgun
 {
 
+class VerticesLayout
+{
+public:
+	void PlaceAttribute(const u32 position, const std::string name);
+
+private:
+	std::map<u32, std::string> attributes_mapping_;
+
+friend class VerticesBuilder;
+};
+
 class VerticesBuilder
 {
+public:
+	explicit VerticesBuilder(const VerticesLayout layout);
+
+	VerticesBuilder & AddAtrribute(const std::string name, void* data);
+	std::unique_ptr<Vertices> Build(const VerticesFormat::Enum format);
+
+private:
+	const VerticesLayout layout_;
 };
 
 }
